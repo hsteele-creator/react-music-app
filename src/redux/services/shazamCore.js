0e95fc3b61c8935f005e19a811711d880ea607d9
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2023-01-23 17:03:24 -0500

    up to working on play and pause functionality

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+  reducerPath: 'shazamCoreApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+    prepareHeaders: (headers) => {
+      headers.set(
+        'X-RapidAPI-Key',
+        '795a905ad3mshc224423d2b21b54p1519a7jsnb481b7f61378'
+      );
+
+      return headers;
+    },
+  }),
+  endpoints: (builder) => ({
+      getTopCharts: builder.query({
+      query: () => '/charts/world',
+    }),
+  }),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
