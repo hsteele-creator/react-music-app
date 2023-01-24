ac04eb1f0475f822c998f41d88ced4c3630806db
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2023-01-24 17:16:51 -0500

    added nav menu

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -9,12 +9,14 @@ import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
 const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
   const dispatch = useDispatch();
+  
   const handlePauseClick = () => {
-
+      dispatch(playPause(false))
   }
 
   const handlePlayClick = () => {
-
+    dispatch(setActiveSong({song, data, i}));
+    dispatch(playPause(true))
   }
 
   return (
